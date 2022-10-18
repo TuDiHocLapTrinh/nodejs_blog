21854329146d2d000315a667cf73fd8b07a19c96
Anh Tu Dang <anhtudang95@gmail.com>
2022-10-18 22:47:14 +0900

    Remove public/css folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { engine, create, ExpressHandlebars } from "express-handlebars";
 import morgan from "morgan";
+// import * as path from "path";
+// import { fileURLToPath } from "url";
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+// console.log(__dirname);
+app.use(express.static("src/public"));
 // HTTP logger
 app.use(morgan("combined"));
 
