e06f0744485e551a2d95ad961bb6b362f1d53f00
Anh Tu Dang <anhtudang95@gmail.com>
2022-11-02 23:17:35 +0900

    connect to db by mongoose

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,22 @@
+const Course = require('../models/Course');
+
 class SiteController {
     // [GET] /news
     index(req, res) {
-        res.render('home')
+        Course.find({}, function (err, courses) {
+            if (!err) {
+                res.json(courses);
+            } else {
+                res.status(400).json({ error: 'ERROR' });
+            }
+        });
+        //res.render('home')
     }
 
     // [GET] /:slug
     search(req, res) {
-        res.render('search')
+        res.render('search');
     }
 }
 
-module.exports = new SiteController()
+module.exports = new SiteController();
