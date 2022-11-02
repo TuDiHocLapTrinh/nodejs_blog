cda929fff1a821f25dc167765fdb8170f776131a
Anh Tu Dang <anhtudang95@gmail.com>
2022-11-02 22:03:41 +0900

    delete folder node_modules

--- src/index.js
+++ src/index.js
@@ -5,36 +5,36 @@
 // import { fileURLToPath } from "url";
 // const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const express = require("express");
-const morgan = require("morgan");
-const handlebars = require("express-handlebars");
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-const route = require("./routes");
+const route = require('./routes')
 
 // console.log(__dirname);
-app.use(express.static("src/public"));
+app.use(express.static('src/public'))
 
 //MiddleWare xmlhtttprequest,
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(express.json());
+    express.urlencoded({
+        extended: true,
+    })
+)
+app.use(express.json())
 // HTTP logger
 //app.use(morgan("combined"));
 
 //Template engine
 //app.engine("hbs", handlebars({ extname: ".hbs" }));
-app.engine(".hbs", handlebars.engine({ extname: ".hbs" }));
-app.set("view engine", ".hbs");
-app.set("views", "src/resources/views");
+app.engine('.hbs', handlebars.engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', 'src/resources/views')
 
-route(app);
+route(app)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    console.log(`Example app listening on port ${port}`)
+})
